c77dc15bacb3cd4fc05059795bdb1ab1dcd32163
Lizzie <lchan217@gmail.com>
2021-02-02 11:07:08 -0700

    set up basics for uploading file

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,24 @@
 class JobsController < ApplicationController
+    def index
+        @jobs = Job.all
+    end
+
+    def show
+    end
+    
+    def new
+        @job = Job.new
+    end
+
+    def create
+        Job.import(params[:job][:file])
+        flash[:notice] = "Jobs uploaded successfully"
+        redirect_to jobs_path
+    end
+
+    private
+
+    def job_params
+        params.require(:job).permit(:title, :company_name, :url, :source)
+    end
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,18 @@
 class Job < ApplicationRecord
+    require 'csv'
+    def self.import(file)
+        CSV.foreach(file.path, headers: true) do |row|
+            job = Job.new
+            job.title = row[1]
+            job.company_name = row[2]
+            job.url = row[3]
+            # job.find(source)
+            job.save
+        end
+    end
+
+    private
+
+    def find_source
+    end
 end
